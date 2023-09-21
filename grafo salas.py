8087c08d1f7a3780de55345a4b3d25f9374fe62b
Andres Infante <a.infante@uc.cl>
2023-09-21 12:43:32 -0300

    grafo salas listo, solo falta AP

--- grafo salas.py
+++ grafo salas.py
@@ -12,14 +12,14 @@ def crear_grafo(path_edges, path_nodos):
     edgelist_pl = pl.read_excel(path_edges)
     nodelist_pl = pl.read_excel(path_nodos)
     new_columns = {col: col.strip() for col in nodelist_pl.columns}
-    # Se sacan las salas de educación, teología y citeduc, y las con menos de 20 de capacidad
+    # Se sacan las salas de educación, teología y citeduc, y las con 30 o menos de capacidad
     # solo se dejan las columnas de nombre y capacidad
     nodelist_pl = (nodelist_pl.rename(new_columns).drop('Location Formal Name')
                    .filter(pl.col("Campus Partition") != '11-Educacion_Ed')
                    .filter(pl.col("Campus Partition") != '12-Educacion_T')
                    .filter(pl.col("Campus Partition") != '53-Salas_Citeduc')
                    .filter(pl.col("Campus Partition") != '55-Salas_Teologia')
-                   .drop('Campus Partition').filter(pl.col("Max Capacity") >= 20))
+                   .drop('Campus Partition').filter(pl.col("Max Capacity") > 30))
     edgelist_pl = (edgelist_pl.filter(pl.col("distancia") != 0))
     lista = []
     for item in nodelist_pl.to_pandas()['Location Name'].values:
